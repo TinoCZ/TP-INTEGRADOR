45e6b6f62bb5ac6ae78b7bc247c389f04a8dc008
Tino <cuchizaninivalen06@gmail.com>
2023-04-14 15:24:17 -0300

    creo que ya estaria

---
+++ app.js
@@ -0,0 +1,8 @@
+
+let type = new Typed(".mucho-texto", {
+    strings: ["Estudio desarrollo web front-end", "Tengo 18 años 😀", "Soy fanatico del freestyle rap", "Me encanta la tecnologia", "Y me interesa mucho la psicologia (mas especificamente los trastornos disociativos)"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 100,
+    loop: true,
+})
